ec02ae598c20eebdc2eeddc949bf270b48121e4d
radamou <radamou@eyrolles.com>
2015-05-26 21:07:45 +0200

    creation du projet seo site

---
+++ seo_site/news_site/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.template.defaultfilters import default
+
+# Create your models here.
+
+class Article(models.Model):
+    title_text = models.CharField(max_length=200);
+    description_text = models.CharField(max_length=200);
+    content_text = models.TextField();
+    public_date = models.DateTimeField('date published');
+    
