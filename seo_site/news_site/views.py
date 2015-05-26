b185caff6e1c66a3c68cbf628288a36c709fa2a3
radamou <radamou@eyrolles.com>
2015-05-27 00:37:17 +0200

    ajout de laffichage

--- seo_site/news_site/views.py
+++ seo_site/news_site/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.context_processors import request
+from django.template import RequestContext, loader
+from .models import Article
+from aptsources.distinfo import Template
 
-# Create your views here.
+def index(request):
+    template = loader.get_template('news/index.html');
+    articles = Article.objects.order_by('-public_date');
+    context = RequestContext(request, {
+    'articles' : articles
+    })
+    return HttpResponse(template.render(context));
