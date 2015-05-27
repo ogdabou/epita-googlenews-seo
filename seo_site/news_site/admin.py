c01f0c1492fa98859e9382fd9e2a5301ea8fc816
Axel <ogdabou@gmail.com>
2015-05-28 01:50:52 +0200

    Added nGram computing class

--- seo_site/news_site/admin.py
+++ seo_site/news_site/admin.py
@@ -4,16 +4,19 @@ from django.contrib import messages
 from .forms import FeedForm
 from .models import Article, Feed
 from .FeedParser import FeedParser
+from .NGram import NGram
 
 
 def update_price(modeladmin, request, queryset):
     feedparser = FeedParser()
+    ngram = NGram()
 
     for feed in queryset:
         articles = feedparser.parse(feed.url)
 
         for article in articles:
             article.save()
+            ngram.computeNGrams(article.description_text, 2)
         modeladmin.message_user(request, ("Successfully crawled %d / %d feeds") % (len(queryset), len(articles)), messages.SUCCESS)
 
 update_price.short_description = 'Crawl RSS feed'
