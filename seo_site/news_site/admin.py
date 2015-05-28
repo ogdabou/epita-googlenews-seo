d17772d0455d32293537577f7e11ee11249142f0
radamou <radamou@eyrolles.com>
2015-05-28 07:47:25 +0200

    add stop word

--- seo_site/news_site/admin.py
+++ seo_site/news_site/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Article
 
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title_text', 'description_text', 'public_date')
     list_filter = ['public_date']
