6b3564f7be01e749cd802ec734ad9546f6ee6fcb
Axel <ogdabou@gmail.com>
2015-05-28 23:08:00 +0200

    removing stopwords anc computing ngrams

--- seo_site/news_site/KeyWordsProcessor.py
+++ seo_site/news_site/KeyWordsProcessor.py
@@ -9,16 +9,18 @@ class KeyWordsProcessor():
     def process(self, feed):
         ngramprocessor = NGram()
         articles = Article.objects.filter(feed_id=feed.id)
+        stopWordProcessor = stopWord()
 
         for article in articles:
             # stop word
-            cleaned = stopWord.stop_little_words(article.description_text)
+            cleaned = stopWordProcessor.stop_little_words(article.description_text)
 
 
             # lemm
 
             # n grams
-            ngrams = ngramprocessor.computeNGrams(article.description_text, 2)
+            ngrams = ngramprocessor.computeNGrams(cleaned, 2)
+            print ngrams
 
         # tf-idf des n-grams
 
